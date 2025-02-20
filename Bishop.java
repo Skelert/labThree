00f1854a265554866771778e1d77993cb070cfc0
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-02-19 21:51:33 -0700

    make makeBishopCoordinates to hold logic
    making the coordinates here allows me to reuse the method in the
    next subclass without how generateCoordinates is call aka without
    a boolean parameter

--- Bishop.java
+++ Bishop.java
@@ -1,7 +1,7 @@
 import java.util.Vector;
 
-public class Bishop extends Piece{
-    
+public class Bishop extends Piece {
+
     public Bishop() {
         this.pieceName = "bishop";
     }
@@ -14,42 +14,60 @@ public class Bishop extends Piece{
         super(color, col, row, pieceName);
     }
 
-    public int[][] generateCoordinates(boolean force1) {
+    public int[][] makeBishopCoordinates(boolean force1) {
         Vector<int[]> vector = new Vector<>();
-        
+
         String letters = "abcdefgh";
         String numbers = "87654321";
-        
+
         int row = numbers.indexOf(this.row);
         int col = letters.indexOf(this.column);
-        
+
         int[][] directions = {
-            {1, -1},  // Top Left
-            {1, 1},   // Top Right
-            {-1, -1}, // Bottom Left
-            {-1, 1}   // Bottom Right
+                { 1, -1 }, // Top Left
+                { 1, 1 }, // Top Right
+                { -1, -1 }, // Bottom Left
+                { -1, 1 } // Bottom Right
         };
-        
+
         for (int[] direction : directions) {
             int x = row, y = col;
             while (true) {
                 x += direction[0];
                 y += direction[1];
-                
-                if(!inBounds(new int[]{x, y})) 
+
+                if (!inBounds(new int[] { x, y }))
                     break;
 
-                vector.add(new int[]{x, y});
+                vector.add(new int[] { x, y });
 
-                if(force1)
+                if (force1)
                     break;
 
             }
 
         }
-        
-        return vectorToArray(vector);
 
+        return vectorToArray(vector);
+    }
+
+    @Override
+    public int[][] generateCoordinates() {
+        return makeBishopCoordinates(false);
+    }
+
+    public static void main(String[] args) {
+        Bishop test = new Bishop("white", "g", "1");
+        System.out.println(test.getColor());
+        System.out.println(test.getColumn());
+        System.out.println(test.getRow());
+
+        int[][] arr = test.generateCoordinates();
+
+        for (int i = 0; i < arr.length; i += 1) {
+            int[] curArr = arr[i];
+            System.out.println("(" + curArr[0] + "," + curArr[1] + ")");
+        }
     }
 
 }
