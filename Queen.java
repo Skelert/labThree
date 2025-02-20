e0cb33c16791a2243806c9c24673c56b0f486c58
Didier <115608922+DogGotSwag@users.noreply.github.com>
2025-02-19 22:04:08 -0700

    complete new update for Queen class

--- Queen.java
+++ Queen.java
@@ -1,156 +1,83 @@
-import java.util.Arrays;
 import java.util.Vector;
 
-//Author: Didier A.L
-
-public class Queen {
-    public String pieceName;
-    public String color;
-    public String column;
-    public String row;
+public class Queen extends Bishop {
 
     public Queen() {
-        this.pieceName = "Queen";
-        this.color = null;
-        this.column = null;
-        this.row = null;
+        this.pieceName = "queen";
     }
 
     public Queen(String color, String col, String row) {
-        this.pieceName = "Queen";
-        this.color = color.toLowerCase();
-        this.column = col.toLowerCase();
-        this.row = row;
+        super(color, col, row, "queen");
     }
 
     public Queen(String color, String col, String row, String pieceName) {
-        this.pieceName = pieceName;
-        this.color = color.toLowerCase();
-        this.column = col.toLowerCase();
-        this.row = row;
+        super(color, col, row, pieceName);
     }
 
-    public void setColumn(String col) {
-        this.column = col.toLowerCase();
-    }
-
-    public void setRow(String row) {
-        this.row = row;
-    }
-
-    public String getColumn() {
-        return this.column;
-    }
-
-    public String getRow() {
-        return this.row;
-    }
-
-    public String getColor() {
-        return this.color;
-    }
-
-    public boolean inBounds(int[] coordinate) {
-        int indexOne = coordinate[0];
-        int indexTwo = coordinate[1];
-        if (indexOne < 0 || indexOne > 7)
-            return false;
-        if (indexTwo < 0 || indexTwo > 7)
-            return false;
-        return true;
-    }
-
-    public int[][] vectorToArray(Vector<int[]> vector) {
-        int[][] newArray = new int[vector.size()][2];
-        for (int i = 0; i < vector.size(); i += 1) {
-            int[] curr = { vector.get(i)[0], vector.get(i)[1] };
-            newArray[i] = curr;
-        }
-        return newArray;
-    }
-
-    public int[][] generateCoordinates() {
+    public int[][] makeRookCoordinates(boolean forceStop) {
         Vector<int[]> vector = new Vector<>();
-
         String letters = "abcdefgh";
         String numbers = "87654321";
-
         int rows = numbers.indexOf(this.row);
         int cols = letters.indexOf(this.column);
 
-        // up down left right
-        for (int i = cols - 1; i >= 0; i -= 1) {
-            int[] left = { rows, i };
-            vector.add(left);
-        }
-        for (int i = cols + 1; i <= 7; i += 1) {
-            int[] right = { rows, i };
-            vector.add(right);
-        }
-        for (int i = rows - 1; i >= 0; i -= 1) {
-            int[] up = { i, cols };
-            vector.add(up);
-        }
-        for (int i = rows + 1; i <= 7; i += 1) {
-            int[] down = { i, cols };
-            vector.add(down);
-        }
+        for (int i = 0; i < 8; i++) {
+            if (i != cols) {
+                int[] coordinate = { rows, i };
+                if (inBounds(coordinate)) {
+                    vector.add(coordinate);
+                }
+            }
 
-        // diagonal movement
-        int copyRows = rows - 1;
-        int copyCols = cols + 1;
-        while (copyRows >= 0 && copyCols <= 7) {
-            int[] topRight = { copyRows, copyCols };
-            vector.add(topRight);
-            copyRows -= 1;
-            copyCols += 1;
+            if (forceStop) {
+                break;
+            }
         }
+        for (int i = 0; i < 8; i++) {
+            if (i != rows) {
+                int[] coordinate = { i, cols };
+                if (inBounds(coordinate)) {
+                    vector.add(coordinate);
+                }
+            }
 
-        copyRows = rows - 1;
-        copyCols = cols - 1;
-        while (copyRows >= 0 && copyCols >= 0) {
-            int[] topLeft = { copyRows, copyCols };
-            vector.add(topLeft);
-            copyRows -= 1;
-            copyCols -= 1;
-        }
-
-        copyRows = rows + 1;
-        copyCols = cols - 1;
-        while (copyRows <= 7 && copyCols >= 0) {
-            int[] bottomLeft = { copyRows, copyCols };
-            vector.add(bottomLeft);
-            copyRows += 1;
-            copyCols -= 1;
-        }
-
-        copyRows = rows + 1;
-        copyCols = cols + 1;
-        while (copyRows <= 7 && copyCols <= 7) {
-            int[] bottomRight = { copyRows, copyCols };
-            vector.add(bottomRight);
-            copyRows += 1;
-            copyCols += 1;
+            if (forceStop) {
+                break;
+            }
         }
 
         return vectorToArray(vector);
+
     }
 
-    public boolean verifyTarget(String column, String row) {
-        String letters = "abcdefgh";
-        String numbers = "87654321";
-        int attackRows = numbers.indexOf(row);
-        int attackCols = letters.indexOf(column.toLowerCase());
+    @Override
+    public int[][] generateCoordinates() {
+        int[][] arr = super.makeBishopCoordinates(false);
+        int[][] arrTwo = makeRookCoordinates(false);
 
-        int[] attackPosition = { attackRows, attackCols };
-        int[][] availableCoordinates = this.generateCoordinates();
+        int length1 = arr.length;
+        int length2 = arrTwo.length;
 
-        for (int i = 0; i < availableCoordinates.length; i += 1) {
-            int[] curr = availableCoordinates[i];
-            if (Arrays.equals(curr, attackPosition)) {
-                return true;
-            }
+        int[][] result = new int[length1 + length2][2];
+
+        System.arraycopy(arr, 0, result, 0, length1);
+        System.arraycopy(arrTwo, 0, result, length1, length2);
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Queen test = new Queen("white", "g", "1");
+        System.out.println(test.getColor());
+        System.out.println(test.getColumn());
+        System.out.println(test.getRow());
+
+        int[][] arr = test.generateCoordinates();
+
+        for (int i = 0; i < arr.length; i += 1) {
+            int[] curArr = arr[i];
+            System.out.println("(" + curArr[0] + "," + curArr[1] + ")");
         }
-        return false;
     }
+
 }
